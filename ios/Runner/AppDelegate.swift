783a79accd916bf09f2c5804f6949548d4ed8e3a
AhsanHabib275 <pandwalay@gmail.com>
2025-04-04 10:57:14 +0500

    first commit

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,53 @@
+// import UIKit
+// import Flutter
+// import FirebaseCore
+// import Firebase
+// import FirebaseMessaging
+
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//     override func application(
+//         _ application: UIApplication,
+//         didFinishLaunchingWithOptions launchOptions:  [UIApplication.LaunchOptionsKey: Any]?
+//     ) -> Bool {
+//         FirebaseApp.configure()
+//         GeneratedPluginRegistrant.register(with: self)
+//         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//     }
+//     override func application (_ application: UIApplication,
+//                                didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+//         Messaging.messaging().apnsToken = deviceToken
+//         print("Token: \(deviceToken)")
+//         super.application (application,
+//                            didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+//     }   
+// }
+
+
+import UIKit
+import Flutter
+import FirebaseCore
+import Firebase
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    GeneratedPluginRegistrant.register(with: self)
+     Messaging.messaging().delegate = self
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                    options: authOptions,
+                    completionHandler: {_, _ in })
+        }
+        application.registerForRemoteNotifications()
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
